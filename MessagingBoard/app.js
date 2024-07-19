c83e8b8601b5bb10776e5c30c41e5502108badc4
cwiggin9 <cwiggins.g@gmail.com>
2024-07-19 14:47:25 -0500

    Created a form view that creates a post request

--- MessagingBoard/app.js
+++ MessagingBoard/app.js
@@ -2,7 +2,11 @@ const express = require('express')
 const app = express()
 const indexRouter = require("./routes/indexRouter");
 const messagesRouter = require("./routes/messagesRouter");
+
+app.use(express.static("public"))
+
 app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: true }));
 
 app.use("/", indexRouter);
 app.use("/new", messagesRouter);
--- MessagingBoard/routes/messagesRouter.js
+++ MessagingBoard/routes/messagesRouter.js
@@ -2,6 +2,6 @@ const { Router } = require("express");
 
 const messagesRouter = Router();
 
-messagesRouter.get("/", (req, res) => res.send("Messages"));
+messagesRouter.get("/", (req, res) => res.render("form"));
 
 module.exports = messagesRouter;
